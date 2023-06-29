4197e797592677c76f5ca9743bd78a360aaed7f2
prozhyvarov <121400043+prozhyvarov@users.noreply.github.com>
2023-06-29 04:53:22 -0600

    update

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -6,18 +6,24 @@ import {
   SearchInput,
   SearchButton,
 } from './SearchBar.styled';
+import Notiflix from 'notiflix';
 
 
 const SearchBar = ({ onSubmit }) => {
   const [inputValue, setInputValue] = useState('');
 
   const handleChange = event => {
-    setInputValue(event.target.value);
+    setInputValue(event.target.value.trim());
   };
 
   const handleSubmit = event => {
     event.preventDefault();
-    setInputValue(inputValue.trim());
+    if (!inputValue) {
+      return Notiflix.Notify.warning(
+        'The search bar cannot be empty. Please type any criteria in the search bar.'
+      );
+     
+    }
     onSubmit(inputValue);
     event.target.reset();
   };
